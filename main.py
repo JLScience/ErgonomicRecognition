f0915f2fd4d7d04acfcdfa204c7d0e86525b89bf
jlscience <julius_lange@web.de>
2019-03-01 21:02:29 +0100

    eye dataset collection added to program flow; program flow variables changed to global; key handling implemented and outsourced from main

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ import cv2 as cv
 
 import engine
 import painter
+import eye_classifier
 
 FPS = 30
 
+# program switches:
+DEBUG = False
+APPLY_VIOLA_JONES = False
+EYE_DATA_GATHERING = True
+KEY_PRESSED = ""
 
-def process_input_and_modify_screen(frame, viola_jones=False, debug=True):
+
+def process_input_and_modify_screen(frame):
 
     # apply mirroring:
     frame = engine.mirror_vertical(frame)
@@ -17,13 +24,13 @@ def process_input_and_modify_screen(frame, viola_jones=False, debug=True):
     key_points, max_scores = engine.calc_keypoints(frame)
 
     # use Viola-Jones algorithm to find faces and eyes:
-    if viola_jones:
+    if APPLY_VIOLA_JONES:
         faces = engine.find_face_viola_jones(frame)
         eyes = []
         for x, y, w, h in faces:
             eyes.append(engine.find_eyes_viola_jones(frame[y:y+h, x:x+w]))
 
-    if debug:
+    if DEBUG:
         # plot keypoints:
         painter.paint_keypoints(frame, key_points, max_scores, 0.1, only_torso=False)
 
@@ -37,12 +44,26 @@ def process_input_and_modify_screen(frame, viola_jones=False, debug=True):
         painter.paint_rect(frame, l_eye[0], l_eye[1], l_eye[2], l_eye[3])
         painter.paint_rect(frame, r_eye[0], r_eye[1], r_eye[2], r_eye[3])
 
-        if viola_jones:
+        # plot frames calculated by viola-jones algorithms:
+        if APPLY_VIOLA_JONES:
             for i, (x, y, w, h) in enumerate(faces):
                 painter.paint_rect(frame, x, y, w, h)
                 for ex, ey, ew, eh in eyes[i]:
                     painter.paint_rect(frame[y:y+h, x:x+w], ex, ey, ew, eh)
 
+    if EYE_DATA_GATHERING:
+        # find and plot eye surrounding:
+        l_eye, r_eye = engine.find_eyes(key_points)
+        lx, ly, ldx, ldy = int(l_eye[0]), int(l_eye[1]), int(l_eye[2]), int(l_eye[3])
+        rx, ry, rdx, rdy = int(r_eye[0]), int(r_eye[1]), int(r_eye[2]), int(r_eye[3])
+        if KEY_PRESSED == 'c':
+            eye_classifier.collect_data(frame[ly+1:ly+ldy, lx+1:lx+ldx], frame[ry+1:ry+rdy, rx+1:rx+rdx],
+                                        eyes_open=False)
+        if KEY_PRESSED == 'o':
+            eye_classifier.collect_data(frame[ly+1:ly+ldy, lx+1:lx+ldx], frame[ry+1:ry+rdy, rx+1:rx+rdx],
+                                        eyes_open=True)
+        painter.paint_rect(frame, l_eye[0], l_eye[1], l_eye[2], l_eye[3])
+        painter.paint_rect(frame, r_eye[0], r_eye[1], r_eye[2], r_eye[3])
 
 
     # check if the posture is incorrect:
@@ -51,7 +72,22 @@ def process_input_and_modify_screen(frame, viola_jones=False, debug=True):
     # show signal if posture is incorrect:
     frame = painter.posture_alert(frame, posture_list)
 
+    # plot key pressed:
+    painter.paint_string(frame, "Key pressed: " + KEY_PRESSED, frame.shape[1]-390, 225)
+    # print(KEY_PRESSED)
     return frame
+
+
+def key_handling(key):
+    global KEY_PRESSED
+    if key == 27:
+        exit(code=0)
+    elif key == ord('c'):
+        KEY_PRESSED = 'c'
+    elif key == ord('o'):
+        KEY_PRESSED = 'o'
+    else:
+        KEY_PRESSED = ""
 
 
 def main():
@@ -72,8 +108,7 @@ def main():
 
         ms_wait = int(1000 / FPS)
         key = cv.waitKey(ms_wait)
-        if key == 27:
-            break
+        key_handling(key)
 
 
 if __name__ == '__main__':
