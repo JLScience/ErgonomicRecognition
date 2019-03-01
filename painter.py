337633673bfe76069f7b5b00605feb230c4a739f
jlscience <julius_lange@web.de>
2019-03-01 09:00:29 +0100

    implemented a simple posture alert

--- painter.py
+++ painter.py
@@ -1,5 +1,6 @@
 # plotting, drawing and manipulating the screen
 
+import numpy as np
 import cv2 as cv
 
 KEYPOINT_DESCRIPTIONS = part_str = ['nose', 'neck', 'Rsho', 'Relb', 'Rwri', 'Lsho', 'Lelb', 'Lwri', 'Rhip',
@@ -19,6 +20,34 @@ def paint_point(frame, x, y, r=5, color=(255, 255, 255)):
     cv.circle(frame, (int(x+0.5), int(y+0.5)), r, color, -1)
 
 
+def paint_rect(frame, x, y, w, h, color=(0, 0, 255)):
+    cv.rectangle(frame, (int(x+0.5), int(y+0.5)), (int(x+w+0.5), int(y+h+0.5)), color, 2)
+
+
 def paint_face_edge(frame, c_x, c_y, face_size_v, face_size_h, angle):
     cv.ellipse(img=frame, center=(int(c_x+0.5), int(c_y+0.5)), axes=(int(face_size_v+0.5), int(face_size_h+0.5)),
                angle=angle, startAngle=0, endAngle=360, color=(255, 255, 255), thickness=2)
+
+
+# TODO: embellish the information display
+def posture_alert(frame, posture_list):
+    notification_list = ['head twisted', 'head leaned forward', 'head leaned sidewards', 'head ducked']
+
+    new_frame = np.zeros((frame.shape[0], frame.shape[1] + 400, frame.shape[2]), dtype=frame.dtype)
+    new_frame[:, :frame.shape[1], :] = frame
+    new_frame[:, frame.shape[1]:, :] = 255 * np.ones((frame.shape[0], 400, frame.shape[2]), dtype=frame.dtype)
+
+    if posture_list[0]:
+        cv.putText(new_frame, notification_list[0], (frame.shape[1]+10, 25),
+                   cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv.LINE_AA)
+    if posture_list[1]:
+        cv.putText(new_frame, notification_list[1], (frame.shape[1]+10, 75),
+                   cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv.LINE_AA)
+    if posture_list[2]:
+        cv.putText(new_frame, notification_list[2], (frame.shape[1]+10, 125),
+                   cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv.LINE_AA)
+    if posture_list[3]:
+        cv.putText(new_frame, notification_list[3], (frame.shape[1]+10, 175),
+                   cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv.LINE_AA)
+
+    return new_frame
