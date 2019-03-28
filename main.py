e0c649c928970816652af218f5bf70f176dde838
jlscience <julius_lange@web.de>
2019-03-28 09:26:17 +0100

    Implemented 3 main screens: start, analysis and info; changed debug behaviour; added calibration process, started and ended by right-left blinking;

--- main.py
+++ main.py
@@ -6,17 +6,33 @@ import engine
 import painter
 import eye_classifier
 
-FPS = 30
+FPS = 60
 
-# program switches:
+# --- program switches:
 RUN = True
+START_SCREEN = True
+ANALYSIS_SCREEN = False
+INFO_SCREEN = False
 DEBUG = False
 APPLY_VIOLA_JONES = False
 EYE_DATA_GATHERING = False
+
+
+# --- global variables
 KEY_PRESSED = ""
+L_EYE_STATUS = []
+R_EYE_STATUS = []
+BLINK_TIMER = 0
+CALIBRATING = False
+CALIBRATING_HEAD_POSITIONS_Y = []
+CALIBRATING_DISTANCE_NOSE_FACE_CENTER = []
+HEAD_THRESHOLD_Y = -1
 
 
 def process_input_and_modify_screen(frame):
+    global L_EYE_STATUS, R_EYE_STATUS, BLINK_TIMER
+    global CALIBRATING, CALIBRATING_HEAD_POSITIONS_Y, CALIBRATING_DISTANCE_NOSE_FACE_CENTER, HEAD_THRESHOLD_Y
+    global START_SCREEN, ANALYSIS_SCREEN, INFO_SCREEN
 
     # apply mirroring:
     frame = engine.mirror_vertical(frame)
@@ -31,56 +47,80 @@ def process_input_and_modify_screen(frame):
         for x, y, w, h in faces:
             eyes.append(engine.find_eyes_viola_jones(frame[y:y+h, x:x+w]))
 
-    if DEBUG:
-        # plot keypoints:
-        painter.paint_keypoints(frame, key_points, max_scores, 0.1, only_torso=False)
-
-        # find and plot face:
-        face_mean_x, face_mean_y, face_size_v, face_size_h, face_angle = engine.find_face(key_points)
-        painter.paint_point(frame, face_mean_x, face_mean_y)
-        painter.paint_face_edge(frame, face_mean_x, face_mean_y, face_size_v, face_size_h, face_angle)
-
-        # find and plot eye surrounding:
-        l_eye, r_eye = engine.find_eyes(key_points)
-        painter.paint_rect(frame, l_eye[0], l_eye[1], l_eye[2], l_eye[3])
-        painter.paint_rect(frame, r_eye[0], r_eye[1], r_eye[2], r_eye[3])
-
-        # plot frames calculated by viola-jones algorithms:
-        if APPLY_VIOLA_JONES:
-            for i, (x, y, w, h) in enumerate(faces):
-                painter.paint_rect(frame, x, y, w, h)
-                for ex, ey, ew, eh in eyes[i]:
-                    painter.paint_rect(frame[y:y+h, x:x+w], ex, ey, ew, eh)
-
     if EYE_DATA_GATHERING:
         # find and plot eye surrounding:
         l_eye, r_eye = engine.find_eyes(key_points, return_mode="")
         lx, ly, ldx, ldy = int(l_eye[0]), int(l_eye[1]), int(l_eye[2]), int(l_eye[3])
         rx, ry, rdx, rdy = int(r_eye[0]), int(r_eye[1]), int(r_eye[2]), int(r_eye[3])
         if KEY_PRESSED == 'c':
-            eye_classifier.collect_data(frame[ly+1:ly+ldy, lx+1:lx+ldx], frame[ry+1:ry+rdy, rx+1:rx+rdx],
-                                        eyes_open=False)
+            eye_classifier.collect_data(frame[ly+1:ly+ldy, lx+1:lx+ldx], frame[ry+1:ry+rdy, rx+1:rx+rdx], eyes_open=False)
         if KEY_PRESSED == 'o':
-            eye_classifier.collect_data(frame[ly+1:ly+ldy, lx+1:lx+ldx], frame[ry+1:ry+rdy, rx+1:rx+rdx],
-                                        eyes_open=True)
+            eye_classifier.collect_data(frame[ly+1:ly+ldy, lx+1:lx+ldx], frame[ry+1:ry+rdy, rx+1:rx+rdx], eyes_open=True)
         painter.paint_rect(frame, l_eye[0], l_eye[1], l_eye[2], l_eye[3])
         painter.paint_rect(frame, r_eye[0], r_eye[1], r_eye[2], r_eye[3])
 
     if RUN:
-        # check if the posture is incorrect:
-        posture_list = engine.check_posture(key_points, max_scores)
+        if START_SCREEN:
+            frame = painter.paint_signal_light(frame, mode='no_init')
+            if KEY_PRESSED == 'a':
+                ANALYSIS_SCREEN = True
+                START_SCREEN = False
 
-        # show signal if posture is incorrect:
-        frame = painter.posture_alert(frame, posture_list)
+        elif ANALYSIS_SCREEN:
 
-        # check whether eyes are opened or closed:
-        lx, ly, ldx, ldy, rx, ry, rdx, rdy = engine.find_eyes(key_points, return_mode='window')
-        l_eye_open, r_eye_open = engine.eye_status(frame[ly+1:ly+ldy, lx+1:lx+ldx], frame[ry+1:ry+rdy, rx+1:rx+rdx])
+            frame = painter.paint_analysis_info(frame, CALIBRATING)
 
-        painter.paint_eye_status(frame, l_eye_open, r_eye_open)
+            # check whether eyes are opened or closed:
+            lx, ly, ldx, ldy, rx, ry, rdx, rdy = engine.find_eyes(key_points, return_mode='window')
+            l_eye_open, r_eye_open = engine.eye_status(frame[ly + 1:ly + ldy, lx + 1:lx + ldx],
+                                                       frame[ry + 1:ry + rdy, rx + 1:rx + rdx])
 
-    # plot key pressed:
-    painter.paint_string(frame, "Key pressed: " + KEY_PRESSED, frame.shape[1] - 390, 225)
+            if DEBUG:
+                painter.paint_eye_status(frame, l_eye_open, r_eye_open)
+                painter.paint_string(frame, str(BLINK_TIMER), 20, 20)
+                painter.paint_keypoints(frame, key_points, max_scores, threshold=0.1, only_torso=True)
+
+            L_EYE_STATUS, R_EYE_STATUS = engine.track_eye_status(L_EYE_STATUS, R_EYE_STATUS, l_eye_open, r_eye_open)
+
+            # check if calibration is started:
+            if not CALIBRATING:
+                CALIBRATING, BLINK_TIMER, L_EYE_STATUS, R_EYE_STATUS = engine.check_right_left_blink(L_EYE_STATUS, R_EYE_STATUS, BLINK_TIMER)
+
+            if CALIBRATING:
+                # store values for calibration:
+                head_x, head_y = engine.get_mean_face_positions(key_points)
+                distance = engine.calibration_get_mean_distance_nose_to_face_center(key_points)
+                CALIBRATING_HEAD_POSITIONS_Y.append(head_y)
+                CALIBRATING_DISTANCE_NOSE_FACE_CENTER.append(distance)
+
+                # check if calibration is done:
+                c_tmp, BLINK_TIMER, L_EYE_STATUS, R_EYE_STATUS = engine.check_right_left_blink(L_EYE_STATUS, R_EYE_STATUS, BLINK_TIMER)
+                if c_tmp:
+                    HEAD_THRESHOLD_Y = engine.calibration_get_threshold(CALIBRATING_HEAD_POSITIONS_Y, CALIBRATING_DISTANCE_NOSE_FACE_CENTER)
+                    CALIBRATING_HEAD_POSITIONS_Y = []
+                    CALIBRATING = False
+                    ANALYSIS_SCREEN = False
+                    INFO_SCREEN = True
+
+                if DEBUG:
+                    painter.paint_point(frame, head_x, head_y, color=(0, 0, 255))
+
+        elif INFO_SCREEN:
+
+            if DEBUG:
+                # show threshold:
+                painter.paint_line(frame, 0, HEAD_THRESHOLD_Y, frame.shape[1], HEAD_THRESHOLD_Y, color=(0, 0, 0))
+
+            # check if the posture is incorrect:
+            posture_list = engine.check_posture(key_points, max_scores, HEAD_THRESHOLD_Y)
+
+            # show signal if posture is incorrect:
+            frame = painter.posture_alert(frame, posture_list)
+
+            # go back to analysis screen if 'a' is pressed:
+            if KEY_PRESSED == 'a':
+                INFO_SCREEN = False
+                ANALYSIS_SCREEN = True
 
     return frame
 
@@ -93,6 +133,10 @@ def key_handling(key):
         KEY_PRESSED = 'c'
     elif key == ord('o'):
         KEY_PRESSED = 'o'
+    elif key == ord('a'):
+        KEY_PRESSED = 'a'
+    elif key == ord('d'):
+        KEY_PRESSED = 'd'
     else:
         KEY_PRESSED = ""
 
